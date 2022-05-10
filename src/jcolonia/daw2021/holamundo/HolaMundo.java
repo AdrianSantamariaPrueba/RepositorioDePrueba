470e7bf1a9d8c312a574e0f5e3bb9fc56f257831
Adrián Santamaría <asr.smr.juandecolonia@gmail.com>
2022-05-10 11:04:02 +0200

    Proyectito de javita

--- src/jcolonia/daw2021/holamundo/HolaMundo.java
+++ src/jcolonia/daw2021/holamundo/HolaMundo.java
@@ -3,7 +3,7 @@ package jcolonia.daw2021.holamundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Banda!");
+		System.out.println("Hola Banda!!");
 
 	}
 
